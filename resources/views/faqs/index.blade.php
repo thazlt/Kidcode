e8f75dafec12ec467c5192f28e645103972befc8
NguyenVoThanhVy <1859050@itec.hcmus.edu.vn>
2020-11-07 00:36:34 +0700

    faq_v2

--- resources/views/faqs/index.blade.php
+++ resources/views/faqs/index.blade.php
@@ -1,9 +1,124 @@
 <?php
 include APPROOT . '/resources/views/inc/header.blade.php';
  ?>
-    <div class="banner-forum">
+    <div class="banner-faq">
         <div class="container-fluid">
-            <h1 class="forum-title">Discussion - Questionádfss and Answers</h1>
+            <h1 class="forum-title">FAQ </h1>
+            <p>Can't find the answer you're looking for? We've shared some of our most frequently asked questions to help you out!</p>
+        </div>
+    </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-3">
+                <div class="nav flex-column nav-pills" id="v-pills-tab" role="tablist" aria-orientation="vertical">
+                    <a class="nav-link active" id="v-pills-home-tab" data-toggle="pill" href="#v-pills-home" role="tab" aria-controls="v-pills-home" aria-selected="true">General</a>
+                    <a class="nav-link" id="v-pills-profile-tab" data-toggle="pill" href="#v-pills-profile" role="tab" aria-controls="v-pills-profile" aria-selected="false">Payment</a>
+                    <a class="nav-link" id="v-pills-messages-tab" data-toggle="pill" href="#v-pills-messages" role="tab" aria-controls="v-pills-messages" aria-selected="false">Account</a>
+                    <a class="nav-link" id="v-pills-settings-tab" data-toggle="pill" href="#v-pills-settings" role="tab" aria-controls="v-pills-settings" aria-selected="false">Contribution</a>
+                </div>
+            </div>
+            <div class="col-lg-9">
+                <div class="tab-content" id="v-pills-tabContent">
+                    <div class="tab-pane fade show active" id="v-pills-home" role="tabpanel" aria-labelledby="v-pills-home-tab">
+                        <div class="accordion">
+                            <div class="accordion-item" id="question1">
+                                <a class="accordion-link" href="#question1">
+                                    Why can't I start a Lesson ? 
+                                    <i class="fas fa-plus"></i>
+                                    <i class="fas fa-minus"></i>
+                                </a>
+                                <div class="answer">
+                                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil maxime mollitia, sint exercitationem vel enim quasi assumenda facere accusamus 
+                                    vero corporis quaerat laboriosam sed at dolorum temporibus iste doloribus quam?</p>
+                                </div>
+                            </div>
+                            <div class="accordion-item" id="question2">
+                                <a class="accordion-link" href="#question2">
+                                    Why can't I start a Lesson ? 
+                                    <i class="fas fa-plus"></i>
+                                    <i class="fas fa-minus"></i>
+                                </a>
+                                <div class="answer">
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil maxime mollitia, sint exercitationem vel enim quasi assumenda facere accusamus 
+                                    vero corporis quaerat laboriosam sed at dolorum temporibus iste doloribus quam?</p>
+                                </div>
+                            </div>
+                            <div class="accordion-item" id="question3">
+                                <a class="accordion-link" href="#question3">
+                                    Why can't I start a Lesson ? 
+                                    <i class="fas fa-plus"></i>
+                                    <i class="fas fa-minus"></i>
+                                </a>
+                                <div class="answer">
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil maxime mollitia, sint exercitationem vel enim quasi assumenda facere accusamus 
+                                    vero corporis quaerat laboriosam sed at dolorum temporibus iste doloribus quam?</p>
+                                </div>
+                            </div>
+                            <div class="accordion-item" id="question4">
+                                <a class="accordion-link" href="#question4">
+                                    Why can't I start a Lesson ? 
+                                    <i class="fas fa-plus"></i>
+                                    <i class="fas fa-minus"></i>
+                                </a>
+                                <div class="answer">
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil maxime mollitia, sint exercitationem vel enim quasi assumenda facere accusamus 
+                                    vero corporis quaerat laboriosam sed at dolorum temporibus iste doloribus quam?</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="tab-pane fade" id="v-pills-profile" role="tabpanel" aria-labelledby="v-pills-profile-tab">
+                    <div class="accordion">
+                            <div class="accordion-item" id="question5">
+                                <a class="accordion-link" href="#question5">
+                                    What payment methods do you accept ? 
+                                    <i class="fas fa-plus"></i>
+                                    <i class="fas fa-minus"></i>
+                                </a>
+                                <div class="answer">
+                                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil maxime mollitia, sint exercitationem vel enim quasi assumenda facere accusamus 
+                                    vero corporis quaerat laboriosam sed at dolorum temporibus iste doloribus quam?</p>
+                                </div>
+                            </div>
+                            <div class="accordion-item" id="question6">
+                                <a class="accordion-link" href="#question6">
+                                What payment methods do you accept ? 
+                                    <i class="fas fa-plus"></i>
+                                    <i class="fas fa-minus"></i>
+                                </a>
+                                <div class="answer">
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil maxime mollitia, sint exercitationem vel enim quasi assumenda facere accusamus 
+                                    vero corporis quaerat laboriosam sed at dolorum temporibus iste doloribus quam?</p>
+                                </div>
+                            </div>
+                            <div class="accordion-item" id="question7">
+                                <a class="accordion-link" href="#question7">
+                                What payment methods do you accept ? 
+                                    <i class="fas fa-plus"></i>
+                                    <i class="fas fa-minus"></i>
+                                </a>
+                                <div class="answer">
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil maxime mollitia, sint exercitationem vel enim quasi assumenda facere accusamus 
+                                    vero corporis quaerat laboriosam sed at dolorum temporibus iste doloribus quam?</p>
+                                </div>
+                            </div>
+                            <div class="accordion-item" id="question8">
+                                <a class="accordion-link" href="#question8">
+                                What payment methods do you accept ? 
+                                    <i class="fas fa-plus"></i>
+                                    <i class="fas fa-minus"></i>
+                                </a>
+                                <div class="answer">
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil maxime mollitia, sint exercitationem vel enim quasi assumenda facere accusamus 
+                                    vero corporis quaerat laboriosam sed at dolorum temporibus iste doloribus quam?</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="tab-pane fade" id="v-pills-messages" role="tabpanel" aria-labelledby="v-pills-messages-tab">sdfsdf.</div>
+                    <div class="tab-pane fade" id="v-pills-settings" role="tabpanel" aria-labelledby="v-pills-settings-tab">.1111111111..</div>
+                </div>
+            </div>
         </div>
     </div>
 
